d999382728a8bed958de076e033fba056a9141d4
Octoxygen <aricebelda@gmail.com>
2023-05-06 23:17:44 +0800

    test

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -5,6 +5,7 @@ use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
+use Image;
 
 class RegisterController extends Controller
 {
