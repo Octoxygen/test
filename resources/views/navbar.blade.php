8574246068ba89eac9ec6c314ff748c00bbd36c5
Octoxygen <aricebelda@gmail.com>
2023-05-06 22:51:31 +0800

    test

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -20,7 +20,7 @@
     <div class="container-right">
         <img src="{{ Session::get('userdata')->image ? asset('public/storage/image/users/'.Session::get('userdata')->image) : asset('svg/icon-user.svg') }}">
         <img src="{{ Session::get('userdata')->image ? asset('storage/image/users/'.Session::get('userdata')->image) : asset('svg/icon-user.svg') }}">
-        <img src="{{ Session::get('userdata')->image ? asset('storage/image/users/'.Session::get('userdata')->image) : asset('svg/icon-user.svg') }}">
+        <img src="{{ Session::get('userdata')->image ? asset('image/users/'.Session::get('userdata')->image) : asset('svg/icon-user.svg') }}">
         <div class="container-user">
             <p class="username" onclick="window.location='{{ url('account') }}'">{{ Session::get('user') ?? 'adfdadfafadaaaaaaaaaaaaaaafaf' }}</p>
             <p class="postcount" onclick="window.location='{{ url('account') }}'">{{ $postnum == 0? 'no posts yet' : $postnum . " posts" }}</p>
